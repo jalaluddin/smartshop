b409ad4d3866cfd521ee699a52d0ed05fa690dd3
Rishad Wahid <rbwahid@gmail.com>
2018-01-11 11:36:14 +0600

    First Commit.
    Layout is created as _LayoutGeneral.cshtml.
    All other partial views in layout is also separated. Please check the layout file first in depth.

---
+++ SmartShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartShop.Web.Startup))]
+namespace SmartShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
