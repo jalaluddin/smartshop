755f599d1e677836037255304697649c1c3cbbfb
Rishad Wahid <rbwahid@gmail.com>
2018-02-17 01:09:17 +0600

    dropdown styling fixed.

--- SmartShop.Web/Areas/Admin/Controllers/DashboardController.cs
+++ SmartShop.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -16,23 +16,7 @@ namespace SmartShop.Web.Areas.Admin.Controllers
             return View();
         }
 
-        public ActionResult AddCategory()
-        {
-            ProductManagementContext db = new ProductManagementContext();
-            ProductCategoryUnitOfWork uow = new ProductCategoryUnitOfWork(db);
-
-            var parent=uow.ProductCategoryRepository.GetByID(new Guid("8606dcda-1830-c335-f537-08d57534d961"));
-
-            ProductCategory pc = new ProductCategory();
-            pc.Name = "Cap2";
-            pc.IsActive = true;
-            pc.ParentCategory = parent;
-
-            uow.ProductCategoryRepository.Add(pc);
-            uow.Save();
-
-            return View();
-        }
+        
 
     }
 }
