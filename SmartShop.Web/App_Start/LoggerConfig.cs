748845633e8c6fe7ce83706da1b485e5e136eccf
mohidul31 <mislam6789@gmail.com>
2018-02-16 22:08:24 +0600

    Log4net Updated

---
+++ SmartShop.Web/App_Start/LoggerConfig.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SmartShop.Web.App_Start
+{
+    public class LoggerConfig
+    {
+        private static ILog log;
+
+        public static ILog Logger
+        {
+            get
+            {
+                if (log == null)
+                {
+                    log = LogManager.GetLogger("UnhandledExceptions");
+                }
+
+                return log;
+            }
+        }
+
+        internal static void RegisterLogger()
+        {
+            // BasicConfigurator replaced with XmlConfigurator.
+
+            XmlConfigurator.Configure(new FileInfo(HttpContext.Current.Server.MapPath("~/Log4net.config")));
+        }
+    }
+}
