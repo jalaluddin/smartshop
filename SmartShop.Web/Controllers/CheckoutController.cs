8bb8b76e204df70194d4276d3407fb27c64c108f
mohidul31 <mislam6789@gmail.com>
2018-02-23 21:43:50 +0600

    Ajax Datatable Updated

--- SmartShop.Web/Controllers/CheckoutController.cs
+++ SmartShop.Web/Controllers/CheckoutController.cs
@@ -7,25 +7,12 @@ using System.Web.Mvc;
 
 namespace SmartShop.Web.Controllers
 {
-    public class checkoutController : Controller
+    public class CheckoutController : Controller
     {
         // GET: Checkout
         public ActionResult Index()
         {
             return View();
         }
-
-        /*public JsonResult GetCodingProblemSubCategory(DataTablesAjaxRequestModel datatableModel, Guid? category)
-        {
-            if (category.HasValue)
-            {
-                SubmitCodingProblemModel model = new SubmitCodingProblemModel();
-                var jsonData = model.GetCodingProblemSubCategoryJson(datatableModel, category.Value);
-
-                return Json(jsonData, JsonRequestBehavior.AllowGet);
-            }
-            else
-                return Json(null, JsonRequestBehavior.AllowGet);
-        }*/
     }
 }
