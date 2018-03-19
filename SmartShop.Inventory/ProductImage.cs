6ec90ed2f4b54ec85beec6c76725b8b1846bf7af
Jalal Uddin <jalal.exe@gmail.com>
2018-03-19 18:41:58 +0600

    added image upload

--- SmartShop.Inventory/ProductImage.cs
+++ SmartShop.Inventory/ProductImage.cs
@@ -5,6 +5,8 @@ namespace SmartShop.Inventory
     public class ProductImage : Entity 
     {
         public string Caption { get; set; }
-        public string ImageUrl { get; set; }        
+        public string ImageUrl { get; set; }    
+        public string OriginalName { get; set; }
+        public string CurrentName { get; set; }
     }
 }
--- SmartShop.Web/Areas/Admin/Models/ProductModel.cs
+++ SmartShop.Web/Areas/Admin/Models/ProductModel.cs
@@ -19,6 +19,7 @@ namespace SmartShop.Web.Areas.Admin.Models
         public int Quantity { get; set; }
         public string Description { get; set; }
         public bool IsNew { get; set; }
+        public HttpPostedFileBase FeaturedImageFile { get; set; }
 
         public ProductModel()
         {
@@ -29,7 +30,28 @@ namespace SmartShop.Web.Areas.Admin.Models
 
         public void AddProduct(string name, double price, Guid productCategoryId, double specialPrice, int quantity, string description, bool isNew)
         {
+            var imageUrl = UploadFeaturedImage(FeaturedImageFile);
             _productManagementService.AddProduct(name, price, productCategoryId, specialPrice, quantity, description, isNew);
+        }
+
+        private ProductImage UploadFeaturedImage(HttpPostedFileBase featuredImageFile)
+        {
+            if(featuredImageFile != null)
+            {
+                string uploadPath = "~/Temp";
+                string originalName = featuredImageFile.FileName;
+                string newName = Guid.NewGuid().ToString().Replace("-", "") + originalName.Substring(originalName.IndexOf('.'));
+                string fullPath = uploadPath + "/" + newName;
+                featuredImageFile.SaveAs(HttpContext.Current.Server.MapPath(fullPath));
+
+                ProductImage image = new ProductImage();
+                image.ImageUrl = fullPath;
+                image.OriginalName = originalName;
+                image.CurrentName = newName;
+
+                return image;
+            }
+            return null;
         }
 
         public void DeleteProduct(Guid? id)
